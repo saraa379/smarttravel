9ef23a6f753ede220f5fa69738acae84c99b7677
sara <saraa379@yahoo.com>
2019-01-28 16:03:59 +0100

    slider updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,19 +11,19 @@ const content = [
 		title: 'Vulputate Mollis Ultricies Fermentum Parturient',
 		description:
 		'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
-		image: require('../img/car8.jpg')
+		image: require('../img/car18.jpg')
 	},
 	{
-		title: 'Tortor Dapibus Commodo Aenean Quam',
+		title: 'Travelling is not as expensive as you think',
 		description:
-		'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.',
-		image: require('../img/car7.jpg')
+		'If you like travelling, lack of money is not an excuse. With ride-sharing, I became able to travel more often and made a lot of friends who love to travel. - Anna, Örebro',
+		image: require('../img/car19.jpg')
 	},
 	{
 		title: 'Phasellus volutpat metus',
 		description:
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.',
-		image: require('../img/car15.jpg')
+		image: require('../img/car4.jpg')
 	}
 ];
 
@@ -40,8 +40,8 @@ class Home extends Component {
 								<Slider autoplay={6000}>
 								  	{content.map((article, index) => <div key={index}
 										style={{ background:  'url(' + article.image + ') no-repeat center center', backgroundSize: 'cover' }}>
-										<h2>{article.title}</h2>
-										<div>{article.description}</div>
+														<h2 className="HeroTitle">{article.title}</h2>
+														<div className="HeroContent">{article.description}</div>
 										</div>)}
 								</Slider>
 						</div>
