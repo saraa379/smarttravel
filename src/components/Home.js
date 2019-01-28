c372a0eb83ff1b8c62e75354066de5ec4b646cd9
sara <saraa379@yahoo.com>
2019-01-28 12:21:48 +0100

    slider updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,25 +4,26 @@ import {actionClickTab} from '../actions/menuActions.js';
 import './Home.css';
 import Slider from 'react-animated-slider';
 import 'react-animated-slider/build/horizontal.css';
+import Car from '../img/car7.jpg';
 //content of the slider
 const content = [
 	{
 		title: 'Vulputate Mollis Ultricies Fermentum Parturient',
 		description:
 		'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
-		image: 'https://i.imgur.com/ZXBtVw7.jpg'
+		image: require('../img/car8.jpg')
 	},
 	{
 		title: 'Tortor Dapibus Commodo Aenean Quam',
 		description:
 		'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.',
-		image: 'https://i.imgur.com/DCdBXcq.jpg'
+		image: require('../img/car7.jpg')
 	},
 	{
 		title: 'Phasellus volutpat metus',
 		description:
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.',
-		image: 'https://i.imgur.com/DvmN8Hx.jpg'
+		image: require('../img/car15.jpg')
 	}
 ];
 
@@ -36,9 +37,9 @@ class Home extends Component {
 		return (
 			<div className="innerWrap">
 						<div className="HomeHero">
-								<Slider autoplay={5000}>
+								<Slider autoplay={6000}>
 								  	{content.map((article, index) => <div key={index}
-										style={{ background: `url('${article.image}') no-repeat center center` }}>
+										style={{ background:  'url(' + article.image + ') no-repeat center center', backgroundSize: 'cover' }}>
 										<h2>{article.title}</h2>
 										<div>{article.description}</div>
 										</div>)}
