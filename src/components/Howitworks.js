c2a47b7f1bcae6f1f3e7e0e8c27b8018abd2d201
sara <saraa379@yahoo.com>
2019-02-11 16:52:30 +0100

    How it works page updated

--- src/components/Howitworks.js
+++ src/components/Howitworks.js
@@ -4,6 +4,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {actionClickTab} from '../actions/menuActions.js';
+import './Howitworks.css';
 
 class Howitworks extends Component {
 	componentDidMount(){
@@ -13,7 +14,72 @@ class Howitworks extends Component {
 
 		return (
 			<div className="innerWrap">
-				 	<p>How it works page</p>
+
+					<div className="HowHero">
+									<h2>Ride Sharing Through Smart Travel</h2>
+									<p>works as easy as ever!</p>
+					</div>
+
+					<div className="HowContent">
+
+							<div className="HowLeft">
+										<h3>Passengers</h3>
+										<div className="HowSectionWrap">
+												<h4>Find a travel</h4>
+												<div className="HowSectionContent">
+														You can find all available upcoming ride posts on the "Search a
+														Ride" page.
+												</div>
+										</div>
+										<p className="stars">*****</p>
+										<div className="HowSectionWrap">
+												<h4>Contact the post owner</h4>
+												<div className="HowSectionContent">
+														Contact information of the post owner shall be in the post decription.
+														That is it! You don't need to register or give your personal information
+														to use ride-sharing service.
+												</div>
+										</div>
+										<p className="stars">*****</p>
+										<div className="HowSectionWrap Last">
+												<h4>Our recommendation</h4>
+												<div className="HowSectionContent">
+														Don't pay anyone beforehand.
+												</div>
+										</div>
+							</div>
+
+							<div className="HowRight">
+										<h3>Drivers</h3>
+										<div className="HowSectionWrap">
+												<h4>Register</h4>
+												<div className="HowSectionContent">
+														In order to post an add for ride sharing,
+														you need fill our one step simplified registration.
+														You can find it at the bottom of the Login page.
+												</div>
+										</div>
+										<p className="stars">*****</p>
+										<div className="HowSectionWrap">
+												<h4>Post a offer</h4>
+												<div className="HowSectionContent">
+														You can post your offer on the "Offer a Ride" page.
+														Please put your contact information for passengers in the
+														description section. Because we won't publish your email or
+														phone number with your add.
+												</div>
+										</div>
+										<p className="stars">*****</p>
+										<div className="HowSectionWrap Last">
+												<h4>Edit you add</h4>
+												<div className="HowSectionContent">
+														If you need to edit your add, do it on the "Profile" page.
+												</div>
+										</div>
+
+							</div>
+
+					</div>
 			</div>
 		)
 	}
